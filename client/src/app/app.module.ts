197d574f68c83bf33e2112306dd4816729f3df1f
Alabi Emmanuel <bobslegend795@gmail.com>
2017-08-15 14:51:07 +0100

    connection to db and added some functionalities in client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+// define routes
+const appRoutes: Routes = [
+  { path: "", component: MainpageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
+const mongoose = require("mongoose");
 
 
 // require local dependencies
 const routes = require("./routes/route");
+const config = require("./config/config");
 
 // define app
 let app = express();
@@ -15,11 +17,29 @@ let app = express();
 // define port
 const PORT = process.env.PORT || 3000;
 
-// add middleware
+// connect to mongoose
+mongoose.connect(config.database);
+
+// connected successuflly to db
+mongoose.connection.on("connected", () => {
+    console.log("Successfully connected to db");
+})
+
+// ADD MIDDLEWARE
 app.use("/", routes);
+// cors
+app.use(cors());
+// body-parser
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json 
+app.use(bodyParser.json());
+// morgan
+app.use(morgan('dev'))
 
 // static dir
 app.use(express.static(path.join(__dirname, "public")));
+
 
 
 // listen
